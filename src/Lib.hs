2f7e886b8149e0e42d4ca529f2950f5de210ef3e
Daniel Bramucci <dbramuccicode@gmail.com>
2017-12-30 01:22:27 -0600

    Wrote function to reverse the list of people into a Map of restruants.

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,19 @@
 module Lib
-    ( someFunc
+    ( potentialPatrons
     ) where
+
+import qualified Data.Map   as M
+import           Data.Maybe (fromMaybe)
+
+type Name = String
+type Resturant = String
+
+data Person = Person Name [Resturant]
+
+potentialPatrons :: [Person] -> M.Map Resturant [Person]
+potentialPatrons people = foldl addToMap M.empty people
+    where addToMap :: M.Map Resturant [Person] -> Person -> M.Map Resturant [Person]
+          addToMap m p@(Person name rests) = foldl (\m r -> M.alter (\x -> Just $ p:fromMaybe [] x) r m) m rests
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"

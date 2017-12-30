393c14b211d99eaaefcbe46c20c5bb7037a1618b
Daniel Bramucci <dbramuccicode@gmail.com>
2017-12-30 03:46:38 -0600

    Finished solution.

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,18 +1,47 @@
-module Lib
-    ( potentialPatrons
-    ) where
+module Lib where
 
+import           Data.List  (sortOn)
 import qualified Data.Map   as M
 import           Data.Maybe (fromMaybe)
 
 type Name = String
 type Resturant = String
 
-data Person = Person Name [Resturant]
+data Person = Person Name [Resturant] deriving Show
 
-potentialPatrons :: [Person] -> M.Map Resturant [Person]
+potentialPatrons :: [Person] -> M.Map Resturant [Name]
 potentialPatrons = foldl perPerson M.empty
-    where perPerson :: M.Map Resturant [Person] -> Person -> M.Map Resturant [Person]
-          perPerson m p@(Person _ rests) = foldl (perResturant p) m rests
-          perResturant :: Person -> M.Map Resturant [Person] -> Resturant -> M.Map Resturant [Person]
-          perResturant p = flip $ M.alter (\x -> Just $ p:fromMaybe [] x)
+    where perPerson :: M.Map Resturant [Name] -> Person -> M.Map Resturant [Name]
+          perPerson m (Person name rests) = foldl (perResturant name) m rests
+
+          perResturant :: Name -> M.Map Resturant [Name] -> Resturant -> M.Map Resturant [Name]
+          perResturant name = flip $ M.alter (\x -> Just $ name:fromMaybe [] x)
+
+filterOutSingles :: [Person] -> [Person]
+filterOutSingles people = people'
+    where people' :: [Person]
+          people' = map (\(Person name rests) -> Person name (filter (\r -> M.notMember r onePersonRest) rests)) people
+
+          onePersonRest :: M.Map Resturant [Name]
+          onePersonRest = M.filter (\patrons -> length patrons == 1) (potentialPatrons people)
+
+possibleAnswers :: [Person] -> [M.Map Resturant [Name]]
+possibleAnswers [] = [M.empty]
+possibleAnswers (Person _ []:people) = possibleAnswers people -- Ignore people with no resturant to go to.
+possibleAnswers (p:people) = do
+  let Person name rests = p
+  r <- rests
+  ans <- possibleAnswers people
+  return $ M.alter (\x -> Just $ name:fromMaybe [] x) r ans
+
+bestSolutions :: [Person] -> [(Int, M.Map Resturant [Name])]
+bestSolutions people = case allSolutions of
+                           [] -> []
+                           (n, _):_ -> takeWhile (\(x, _)
+                            -> x == n) allSolutions
+  where allSolutions = reverse . map (\x -> (numberOfHappy x, x)) $
+                        sortOn numberOfHappy $
+                        possibleAnswers . sortOn (\(Person _ rs) -> length rs) . filterOutSingles $ people
+
+numberOfHappy :: M.Map Resturant [Name] -> Int
+numberOfHappy = M.foldl' (\s x -> if length x > 1 then s + length x else s) 0

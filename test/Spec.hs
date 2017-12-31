dffb8cbbee03721333e75146fe9e1d2a8917c245
Daniel Bramucci <dbramuccicode@gmail.com>
2017-12-31 00:58:30 -0600

    Added another simple test.

--- test/Spec.hs
+++ test/Spec.hs
@@ -9,6 +9,17 @@ main = hspec $ do
     describe "Find best plans" $ do
         it "One person has no valid plan" $ do
             property $ \n r -> bestSolutions [Person n r] `setEqual` [M.empty]
+        it "Two people with no resturants in common have an empty plan." $ do
+            property $ \n1 r1 n2 r2 ->
+              S.intersection (S.fromList r1) (S.fromList r2) == S.empty ==>
+              bestSolutions [Person n1 r1, Person n2 r2] `setEqual` [M.empty]
+        it "Two people with one resturants in common will \
+            \go to that resturant" $ do
+             property $ \n1 r1 n2 r2 c->
+               S.intersection (S.fromList r1) (S.fromList r2) == S.empty ==>
+               let sol = bestSolutions [Person n1 (c:r1), Person n2 (c:r2)]
+               in sol `setEqual` [M.fromList [(c, [n1, n2])]] ||
+                  sol `setEqual` [M.fromList [(c, [n2, n1])]]
     describe "Possible Bugs" $ do
         it "One person can't go if they list their choice twice" $ do
             bestSolutions [Person "Bob" ["IHOP", "IHOP"]] `setEqual` [M.empty]

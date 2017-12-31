5ecc8e4454360f65ebd96515e464c5332cda5353
Daniel Bramucci <dbramuccicode@gmail.com>
2017-12-31 00:08:34 -0600

    Added simple property test.

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,12 @@
+import qualified Data.Map        as M
+import qualified Data.Set        as S
+import           Lib
+import           Test.Hspec
+import           Test.QuickCheck
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $ do
+    describe "Find best plans" $ do
+        it "One person has no valid plan" $ do
+            property $ \n r -> bestSolutions [Person n r] `setEqual` [M.empty]
+    where setEqual a b = S.fromList a == S.fromList b

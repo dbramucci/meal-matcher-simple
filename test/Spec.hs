4afa6dca46b390af3a131b07ba2768775f59e292
Daniel Bramucci <dbramuccicode@gmail.com>
2017-12-31 00:38:16 -0600

    Added additional tests.

--- test/Spec.hs
+++ test/Spec.hs
@@ -9,4 +9,21 @@ main = hspec $ do
     describe "Find best plans" $ do
         it "One person has no valid plan" $ do
             property $ \n r -> bestSolutions [Person n r] `setEqual` [M.empty]
+    describe "Possible Bugs" $ do
+        it "One person can't go if they list their choice twice" $ do
+            bestSolutions [Person "Bob" ["IHOP", "IHOP"]] `setEqual` [M.empty]
+            `shouldBe` True
+        it "This shouldn't fail this case mean towards greedy algorithms" $ do
+            bestSolutions [Person "Catherine" ["Taco Bell"]
+                         , Person "Bruce" ["McDonalds", "Taco Bell"]
+                         , Person "Adam" ["McDonalds"]
+                         , Person "Ellis" ["Taco Bell", "Long John Silvers"]
+                         , Person "Ferris" ["Long John Silvers"]
+                         , Person "Geralt" ["Long John Silvers"]]
+                         `setEqual` [M.fromList [
+                              ("Long John Silvers", ["Ferris", "Geralt"])
+                            , ("McDonalds", ["Bruce","Adam"])
+                            , ("Taco Bell", ["Catherine", "Ellis"])
+                            ]]
+            `shouldBe` True
     where setEqual a b = S.fromList a == S.fromList b
